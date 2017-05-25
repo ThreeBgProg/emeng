9a1d378ea8aff40918a2de1b5efd9e47082c46bc
17727695227 <13124937709@163.com>
2017-05-25 23:38:59 +0800

    友情链接

---
+++ src/main/java/com/huiming/emeng/controller/LinksController.java
@@ -0,0 +1,70 @@
+package com.huiming.emeng.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+ 
+import com.huiming.emeng.model.Links;
+import com.huiming.emeng.service.LinksService;
+
+@Controller
+public class LinksController {
+
+	@Autowired
+	private LinksService linksService;
+	
+	@RequestMapping("insert")
+	public String insert(Links links,Model model){
+		
+		int result = linksService.insert(links);
+		System.out.println("您成功插入"+result+"条友情链接");
+		
+		return null;
+	}
+	
+	@RequestMapping("insertSelect")
+	public String insertSelect(Links links,Model model){
+		
+		int result = linksService.insertSelective(links);
+		System.out.println("您成功插入"+result+"条友情链接");
+		
+		return null;
+	}
+	
+	@RequestMapping("selectPK")
+	public Object selectByPrimaryKey(@RequestParam("id") Integer id,Model model){
+		
+		Links links = linksService.selectByPrimaryKey(id);
+		model.addAttribute("links", links);
+		return links;
+	}
+	
+	@RequestMapping("deletePK")
+	public String deleteByPrimaryKey(@RequestParam("id") Integer id,Model model){
+		
+		int result = linksService.deleteByPrimaryKey(id);
+		System.out.println("您成功删除"+result+"条友情链接");
+		return null;
+		
+	}
+	
+	@RequestMapping("updPK")
+	public String updateByPrimaryKey(Links links,Model model){
+		
+		int result = linksService.updateByPrimaryKey(links);
+		System.out.println("您成功更新"+result+"条友情链接");
+		return null;
+	}
+	
+	@RequestMapping("updPKSelect")
+	public String updateByPrimaryKeySelect(Links links,Model model){
+		
+		int result = linksService.updateByPrimaryKeySelective(links);
+		System.out.println("您成功更新"+result+"条友情链接");
+		return null;
+	}
+	
+	
+}
