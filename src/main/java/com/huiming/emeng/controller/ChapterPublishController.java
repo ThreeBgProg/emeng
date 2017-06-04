2efb31d9f473cf8b503bdfd6430147334897622e
LeoMs <leoms1996@163.com>
2017-06-04 15:38:45 +0800

    所有文章的增删查改实现

--- src/main/java/com/huiming/emeng/controller/ChapterPublishController.java
+++ src/main/java/com/huiming/emeng/controller/ChapterPublishController.java
@@ -1,5 +1,6 @@
 package com.huiming.emeng.controller;
 
+import com.huiming.emeng.annotation.MappingDescription;
 import com.huiming.emeng.mapper.ChapterMapper;
 import com.huiming.emeng.model.Chapter;
 import com.huiming.emeng.model.Passage;
@@ -19,23 +20,27 @@ public class ChapterPublishController {
     private ChapterMapper chapterMapper;
 
     //显示所有章节
+    @MappingDescription("显示所有章节")
     @RequestMapping("/list/chapter")
     public List<Chapter> showChapter(@RequestParam("lessonId") Integer lessonId){
         return chapterMapper.selectAllChapterFromLesson(lessonId);
     }
     //新增章节
+    @MappingDescription("增加章节")
     @RequestMapping(value = "/insert/chapter",method = RequestMethod.POST)
     public int insertChapter(@RequestBody Chapter chapter){
         return chapterMapper.insertSelective(chapter);
     }
 
     //删除章节
+    @MappingDescription("删除章节")
     @RequestMapping(value = "/delete/chapter",method = RequestMethod.DELETE)
     public int deleteChapter(@RequestParam("chapterId") Integer chapterId){
         return chapterMapper.deleteByPrimaryKey(chapterId);
     }
 
     //更新章节
+    @MappingDescription("更新章节")
     @RequestMapping(value = "/update/chapter",method = RequestMethod.PUT)
     public int deleteChapter(@RequestBody Chapter chapter){
         return chapterMapper.updateByPrimaryKeySelective(chapter);
